982a20be93abdb49e54b4acbd4a1aa45ab3f854b
lyricc <lyricc@sina.cn>
2019-12-06 07:48:34 +0800

    update

--- MsgProtocol.h
+++ MsgProtocol.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <stdint.h>
 #include <string>
-
+#include <time.h>
+#include "byte_buffer.h"
 
 enum class CompressType
 {
@@ -16,7 +17,7 @@ struct MsgHeader
     int64_t     tm;
     int32_t     cookie;
     int32_t     compress;
-    int32_t     uncompresize;
+    int32_t     rawsize;
     int32_t     length;
 };
 
@@ -47,22 +48,35 @@ public:
     {
         MsgHeader*header = (MsgHeader*)msg;
         mHeader = *header;
+
+        mRawData.SetData((const uint8_t*)&mHeader,sizeof(MsgHeader));
+        
         switch (mHeader.compress)
         {
         case (int)CompressType::NONE:
-            
+            mRawData.Append((const uint8_t*)msg + sizeof(MsgHeader),len - sizeof(MsgHeader));
             break;
         
         default:
             break;
-        }
-
-
-
-
-
+        }        
     }
+
+    size_t Serialize(int msgid,const char*msg,size_t length)
+    {
+        Reset();
+        mHeader.msgid = msgid;
+        mHeader.tm = time(NULL);
+        mHeader.rawsize = length;
+        mHeader.length = length;
+        mRawData.SetData((const uint8_t*)&mHeader,sizeof(MsgHeader));
+        mRawData.Append((const uint8_t*)msg,length);
+        return mRawData.size();
+    }
+
+
 protected:
     MsgHeader mHeader;
     std::string mMsgRaw;
+    ByteBuffer mRawData;
 };
